aa73ef24ec91d91a430ee90d50ef1beaefd85951
li-alan <lialan478@yahoo.com>
2021-11-10 02:18:39 -0800

    Create 09-Java End-of-file.java

---
+++ Solutions/09-Java End-of-file.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = 0;
+        while(scan.hasNextLine()){
+            System.out.println(++i +" "+scan.nextLine());
+        }
+    }
+}
